7fc17ca5cc1e34f59326b2685a5d9e60443d0ca0
Carlos Bazzo <carlos.bazzo@unoesc.edu.br>
2022-05-11 22:50:58 -0300

    A1.3 321820

--- Questão 4 - Resto da divisão.cpp
+++ Questão 4 - Resto da divisão.cpp
@@ -1,32 +1,29 @@
 // Questão 4 - resto da divisão
 
-#include<stdio.h>
-#include<stdlib.h>
 #include <stdio.h>
 
-int main ()
-{
- 
-    float num1, num2, resto; // variavéis float para que seja possivel trabalhar com numeros 'quebrados'
-
-    printf("Informe o primeiro valor: \n");    //escanear os valores 
-    scanf("%f", &num1);
-    printf("Informe o segundo valor: \n");
-    scanf("%f", &num2);
-
-    if(num1 == 0 || num2 == 0)      // já encerrando o programa caso a divisão= x/0
-    {
-        printf("Nao eh possível realizar divisao por zero!!! \n");
-    }
-
-    else if (num1 > num2)     
-	    {
-        resto = num1 % num2;
-    } else  {
-        resto = num2 % num1;
-    }
-    printf("O resto da divisão é %d\n", resto);
-
-    return 0;
-
+int main()
+   {
+   	
+   	
+	int num1, num2, resto;
+	
+	
+    printf("Digite o primeiro valor:\n");   //escanear os valores 
+	scanf("%i", &num1);
+	
+	printf("Digite o segundo valor:\n");
+	scanf("%i", &num2);
+	
+		if(num1==0||num2==0){
+	printf("Nao eh possivel realizar divisao por zero\n");   // já encerrando o programa caso a divisão= x/0
+		return 0;
+	}	else if(num1>num2||num1==num2){
+		resto = num1%num2;
+	}
+	else if(num1<num2){
+		resto = num2%num1;
+	}
+	printf("O resto da divisao eh: %i\n", resto);
+  return 0;
 }
